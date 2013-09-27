6d4aff049153ec492575a786bc27872c6593ec12
Justin Chang <justin.chang@blackbaud.com>
2013-09-27 13:14:09 -0500

    Initial Commit for rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BugsMetric::Application.config.secret_token = 'e2746824c13522968b8b9ab80127d1cc235b2dd8490f1e8c7bb1ab598eeee83427332ac7f75d616a4a82a638453b0a1fb888abe3c3cfd9e55d27139024316463'
