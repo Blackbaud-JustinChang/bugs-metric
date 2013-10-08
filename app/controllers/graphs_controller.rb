46fce562fadab7dc8bc9e576dfa714d25acd3b0a
kyle.martinez <kmartinez@convio.com>
2013-10-08 15:57:31 -0500

    click bar graph to go to bugzilla

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -33,6 +33,8 @@ class GraphsController < ApplicationController
 
   def remove_graph
     graph = Graph.find(params[:id])
+    remove_graph_cookie(graph.name) if cookies[:graph_info]
+    remove_url_cookie(graph.name)   if cookies[:url_info]
     graph.destroy
     @graphs = Graph.all
 
@@ -45,13 +47,26 @@ class GraphsController < ApplicationController
 
   private
 
+  def remove_url_cookie(name)
+    urls = eval(cookies[:url_info])
+    urls.delete(name)
+    cookies[:url_info] = urls.to_s
+  end
+
+  def remove_graph_cookie(name)
+    json_cookie = JSON.parse(cookies[:graph_info])
+    json_cookie.delete(name)
+    cookies[:graph_info] = json_cookie.to_json
+  end
+
   def generate_search(search_params)
     "&short_desc=%28#{search_params.split(",").map(&:strip).join('%7C').gsub(' ', '%20')}%29&short_desc_type=regexp"
   end
 
-  def show_graph params
+  def show_graph params=nil
     @graph_query = params if params.values.select { |x| x.nil? }.empty?
     @bugzilla_bugs = {}
+    @bugzilla_urls = {}
     begin_time = Time.now
     if @graph_query and @graph_query[:start_date] < @graph_query[:end_date]
       cookies.delete :graph_info
@@ -60,8 +75,10 @@ class GraphsController < ApplicationController
         search = generate_search(graph.search)
         product = "&product=#{@graph_query[:product]}"
         date_param = date_string(@graph_query[:start_date], @graph_query[:end_date])
-        bugzilla_bug_ids = BugzillaHelper.bug_id_by_url(bugzilla_url + search + date_param + product)
+        bugzilla_url = bugzilla_url + search + date_param + product
+        bugzilla_bug_ids = BugzillaHelper.bug_id_by_url(bugzilla_url)
         @bugzilla_bugs[graph.name] = bugzilla_bug_ids.size
+        @bugzilla_urls[graph.name] = bugzilla_url
 
         # API way
         @bugzilla_bugs_by_date[graph.name] = {}
@@ -73,12 +90,20 @@ class GraphsController < ApplicationController
         end
       end
       cookies[:graph_info] = @bugzilla_bugs_by_date.to_json
-    elsif cookies[:graph_info]
-      @bugzilla_bugs_by_date = JSON.parse(cookies[:graph_info])
-      @bugzilla_bugs_by_date.each do |key, value|
-        @bugzilla_bugs[key] = value.values.reduce(:+)
+      cookies[:url_info] = @bugzilla_urls.to_s if !@bugzilla_urls.empty?
+    elsif cookies[:graph_info] || cookies[:url_info]
+      if cookies[:graph_info]
+        @bugzilla_bugs_by_date = JSON.parse(cookies[:graph_info])
+        @bugzilla_bugs_by_date.each do |key, value|
+          @bugzilla_bugs[key] = value.values.reduce(:+)
+        end
+      end
+
+      if cookies[:url_info]
+        @bugzilla_urls = eval(cookies[:url_info])
       end
     end
+
     create_graph unless @bugzilla_bugs_by_date.empty?
     @total_time = Time.now - begin_time
   end
@@ -111,6 +136,14 @@ class GraphsController < ApplicationController
       f.options[:chart][:defaultSeriesType] = "column"
       f.options[:xAxis] = {:plot_bands => "none", :title => {:text => "Search"}, :categories => @bugzilla_bugs.keys}
       f.options[:yAxis][:title] = {:text => "Answers"}
+      if !@bugzilla_urls.empty?
+      f.options[:plotOptions] = {
+          :column => { :events => { :click => %|function(x) {
+            var urls = #{@bugzilla_urls.values}
+            window.open(urls[x.point.x]); }|.js_code } }
+      }
+      end
+
     end
 
     @line_graph = LazyHighCharts::HighChart.new('graph') do |f|
